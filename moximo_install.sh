be18b176eb5b3d39625082d6481e90fa0ccd487a
Ivan Chavero <ichavero@redhat.com>
2016-11-08 16:07:48 -0700

    Fix syntax errors

--- moximo_install.sh
+++ moximo_install.sh
@@ -10,44 +10,48 @@ HD=$2
 HD_GEOMETRY="moximo_partitions.sfdisk"
 #TODO fix this paths
 # images are in /home/sotolitoLabs at sotolitolabs.com
+BOOTFS="/home/ichavero/sotolitoLabs/moximo-images/fs/moximo-bootfs.tar.gz"
 ROOTFS="/home/ichavero/sotolitoLabs/moximo-images/fs/moximo-rootfs.tar.gz"
 VARFS="/home/ichavero/sotolitoLabs/moximo-images/fs/moximo-varfs.tar.gz"
+IMAGE="../fedora-images/Fedora-Server-armhfp-24-1.2-sda.raw.xz"
 
-if [ ${SD} == "" ]; then
+if [ "${SD}" == "" ]; then
     echo "Missing SD parameter"
     exit 0;
 fi
 
-if [ ${HD} == "" ]; then
+if [ "${HD}" == "" ]; then
     echo "Missing Hard Drive parameter"
     exit 0;
 fi
 
-a
+
 echo "Writing image to SD: ${SD}"
-fedora-arm-image-installer -y --image=${IMAGE} --target=Cubietruck --media=${SD}
+#fedora-arm-image-installer -y --image=${IMAGE} --target=Cubietruck --media=${SD}
 
 echo "Creating HD partitions"
 sfdisk $HD < $HD_GEOMETRY
 mkswap "${HD}1"
-mkfs.ext4 -fy "${HD}2"
-mkfs.xfs -fy "${HD}3"
+mkfs.ext4 -F -F "${HD}2"
+mkfs.xfs -f "${HD}3"
 
 # make temp mount points
-mkdir /tmp/{moximo_sd_boot,moximo_root,moximo_var} &> /dev/null
-mount "${SD}1" /tmp/moximo_sd_boot &> /dev/null
-sed -i 's/\sroot=.*\s/ root=/dev/sda2 /' /tmp/moximo_sd_boot/extlinux/extlinux.conf
+mkdir /tmp/{moximo_sd_boot,moximo_root,moximo_var}
+echo "mount ${SD}p1 /tmp/moximo_sd_boot"
+mount "${SD}p1" /tmp/moximo_sd_boot &> /dev/null
+tar --strip-components=1 -xf ${BOOTFS} -C /tmp/moximo_sd_boot
+#sed -i 's/\sroot=.*\s/ root=\/dev\/sda2 /' /tmp/moximo_sd_boot/extlinux/extlinux.conf
 
-echo "Extracting root filesystem: $ROOTFS"
+echo "Extracting root filesystem ${HD}2: $ROOTFS"
 mount "${HD}2" /tmp/moximo_root
-tar -xvf ${ROOTFS} -C /tmp/moximo_root
+tar -xf ${ROOTFS} -C /tmp/moximo_root
 
-echo "Extracting var filesystem: $VARFS"
-mount "${HD}2" /tmp/moximo_var
-tar -xvf ${VARFS} -C /tmp/moximo_var
+echo "Extracting var filesystem ${HD}3: $VARFS"
+mount "${HD}3" /tmp/moximo_var
+tar --strip-components=1 -xf ${VARFS} -C /tmp/moximo_var
 
-echo "DEBUG exiting before unmounting"
-exit
+#echo "DEBUG exiting before unmounting"
+#exit
 
 
 echo "Unmounting filesystems"
